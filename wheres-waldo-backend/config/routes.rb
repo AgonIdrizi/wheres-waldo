8bf82a01564772a3703c292f8c50a9eb09239774
AgonIdrizi <agon.88@hotmail.com>
2019-06-04 17:19:26 +0200

    add scores controller

--- wheres-waldo-backend/config/routes.rb
+++ wheres-waldo-backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'scores/index'
+  get 'scores/create'
   get 'players/create'
   resources :puzzles , only: [:index]
   resources :players , only: [:create]
+  resources :scores , only: [:create, :index]
   get "puzzle-character-locations/:id", to: "puzzles#find_puzzle_character_location"
 end
